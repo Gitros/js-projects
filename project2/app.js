4249e48e97d5bdf5832fbe8a4a79dcf0ae060487
Gitros <31899790+Gitros@users.noreply.github.com>
2022-07-21 13:26:42 +0200

    project 2

--- project2/app.js
+++ project2/app.js
@@ -8,13 +8,21 @@ const btns = document.querySelectorAll(".btn");
 btns.forEach(function (btn) {
     btn.addEventListener("click", function(e){
         const styles = e.currentTarget.classList;
-        console.log(e.currentTarget.classList);
         if (styles.contains("decrease")) {
             count--;
         } else if (styles.contains('increase')){
             count++;
         } else {
             count = 0;
+        }
+        if(count>0) {
+            value.style.color = "green";
+        }
+        if(count<0) {
+            value.style.color = "red";
+        }
+        if(count === 0) {
+            value.style.color = "black";
         }
         value.textContent = count;
     });
